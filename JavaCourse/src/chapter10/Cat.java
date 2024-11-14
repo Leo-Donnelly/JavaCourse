eaefd4cfcb90fda172ad77f79517792da0231b6a
Leo-Donnelly <leo.donnelly@2itesting.com>
2024-11-14 16:34:34 +0000

    Add files via upload

---
+++ JavaCourse/src/chapter10/Cat.java
@@ -0,0 +1,12 @@
+package chapter10;
+
+public class Cat extends Animal{
+
+	public void makeSound() {
+		System.out.println("MEOW");
+	}
+	
+	public void scratch() {
+		System.out.println("Scratch");
+	}
+}
